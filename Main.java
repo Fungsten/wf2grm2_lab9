f6efd1684f09932686b2ce3d9e9ded63b5e81166
i-am-a-turtle <grm2@williams.edu>
2017-04-26 13:20:30 -0400

    I forgot this

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Vector;
+import java.util.Random;
+import structure5.*;
+
+public class Main{
+
+public static void main(String args[]){
+  Customer a = new Customer(1, 10, "Grace");
+  Customer b = new Customer(2, 10, "Will");
+
+  System.out.println(a.toString());
+  System.out.println(a.compareTo(b));
+  System.out.println(b.toString());
+
+}
+}
